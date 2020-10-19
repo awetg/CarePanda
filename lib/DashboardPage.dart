949a4e90936b50f8e9cb226f6508f11e198791c7
Janne Suotsalo <janne.suotsalo@gmail.com>
2020-10-19 22:14:21 +0300

    working bottom nav bar

---
+++ lib/DashboardPage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DashBoardPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: EdgeInsets.all(25.0),
+        child: Text('Dashboard', style: TextStyle(fontSize: 36.0)));
+  }
+}
---
+++ lib/SettingsPage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SettingsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: EdgeInsets.all(25.0),
+        child: Text('Settings', style: TextStyle(fontSize: 36.0)));
+  }
+}
