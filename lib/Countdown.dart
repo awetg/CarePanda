a323fb1a3aa188b2047f85f7c392946651fce3ce
Janne Suotsalo <janne.suotsalo@gmail.com>
2020-10-21 18:05:22 +0300

    added more time on countdown and improved layout

--- lib/Countdown.dart
+++ lib/Countdown.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
+import 'dart:developer';
+import 'package:carePanda/globals.dart' as globals;
 
 class WeekCountdown extends StatefulWidget {
   @override
@@ -7,32 +9,34 @@ class WeekCountdown extends StatefulWidget {
 }
 
 class _WeekCountdownState extends State<WeekCountdown> {
-  Timer _timer;
-  DateTime _currentTime;
+  Timer timer;
+  DateTime currentTime;
 
   @override
   void initState() {
     super.initState();
-    _currentTime = DateTime.now();
-    _timer = Timer.periodic(Duration(seconds: 1), _onTimeChange);
+    currentTime = DateTime.now();
+    timer = Timer.periodic(Duration(seconds: 1), onTimeChange);
   }
 
   @override
   void dispose() {
-    _timer.cancel();
+    timer.cancel();
     super.dispose();
   }
 
-  void _onTimeChange(Timer timer) {
+  void onTimeChange(Timer timer) {
     setState(() {
-      _currentTime = DateTime.now();
+      currentTime = DateTime.now();
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    final nextWednesday = calculateNextWednesday(_currentTime);
-    final remaining = nextWednesday.difference(_currentTime);
+    final textColor = Color(0xff027DC5);
+    final newTime = currentTime.subtract(new Duration(hours: 15));
+    final nextWednesday = calculateNextWednesday(newTime);
+    final remaining = nextWednesday.difference(newTime);
 
     final days = remaining.inDays;
     final hours = remaining.inHours - remaining.inDays * 24;
@@ -47,7 +51,12 @@ class _WeekCountdownState extends State<WeekCountdown> {
       formattedRemaining = '$hours : $minutes : $seconds';
     }
 
-    return Text(formattedRemaining, style: TextStyle(fontSize: 36.0));
+    if (days == 0 && hours == 0 && minutes == 0 && seconds == 0) {
+      globals.hasQuestionnaire = true;
+    }
+
+    return Text(formattedRemaining,
+        style: TextStyle(fontSize: 36.0, color: textColor));
   }
 }
 
