7ddbf49ab4fc0b899fb66c862fd639915f3c42dc
Janne Suotsalo <janne.suotsalo@gmail.com>
2020-11-03 13:33:42 +0200

    small changes

--- lib/PushNotifHandler.dart
+++ lib/PushNotifHandler.dart
@@ -7,31 +7,26 @@ class PushNotificationHandler {
   final FirebaseMessaging _fcm = FirebaseMessaging();
 
   Future initialise() async {
-    var _storageService = locator<LocalStorageService>();
-
     _fcm.configure(
       // When the app is open and it receives a push notification
       onMessage: (Map<String, dynamic> message) async {
         log("onMessage: $message");
-        _storageService.hasQuestionnaire = true;
       },
 
       // When app is completely closed and launched again
       onLaunch: (Map<String, dynamic> message) async {
         log("onLaunch: $message");
-        _storageService.hasQuestionnaire = true;
       },
 
       // When the app is opened from the background from the push notification
       onResume: (Map<String, dynamic> message) async {
         log("onResume: $message");
-        _storageService.hasQuestionnaire = true;
       },
     );
 
-    /*_fcm.getToken().then((String token) {
+    _fcm.getToken().then((String token) {
       assert(token != null);
       log(token);
-    });*/
+    });
   }
 }
