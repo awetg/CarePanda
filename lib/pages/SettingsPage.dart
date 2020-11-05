a3b044e3bbc4783ad27c2f4d49897a6bab784434
Awet Ghebreslassie <awetghebreslassie@gmail.com>
2020-11-05 21:33:21 +0000

    settings page (#10)
    Co-authored-by: Oliver Östman <oliver.ostman@metropolia.fi>

--- lib/pages/SettingsPage.dart
+++ lib/pages/SettingsPage.dart
@@ -3,8 +3,123 @@ import 'package:flutter/material.dart';
 class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Settings',
+      home: Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+            backgroundColor: Colors.white,
+            title: const Text('Settings',
+                style: TextStyle(color: Color(0xff027DC5)))),
+        body: MyStatefulWidget(),
+      ),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  MyStatefulWidget({Key key}) : super(key: key);
+
+  @override
+  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
+}
+
+String dropdownValue = 'English';
+String themeValue = 'Light';
+bool isSwitched = true;
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  @override
+  Widget build(BuildContext context) {
     return Container(
         padding: EdgeInsets.all(25.0),
-        child: Text('Settings', style: TextStyle(fontSize: 36.0)));
+        child: Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            //crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Column(children: [
+                Text('App Settings',
+                    style:
+                        TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold)),
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('Language', style: TextStyle(fontSize: 20.0)),
+                      DropdownButton<String>(
+                        value: dropdownValue,
+                        icon: Icon(Icons.arrow_downward),
+                        iconSize: 24,
+                        elevation: 16,
+                        style: TextStyle(color: Colors.deepPurple),
+                        underline: Container(
+                          height: 2,
+                          color: Colors.deepPurpleAccent,
+                        ),
+                        onChanged: (String newValue) {
+                          setState(() {
+                            dropdownValue = newValue;
+                          });
+                        },
+                        items: <String>['English', 'Swedish', 'Finnish']
+                            .map<DropdownMenuItem<String>>((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child:
+                                Text(value, style: TextStyle(fontSize: 20.0)),
+                          );
+                        }).toList(),
+                      )
+                    ]),
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('Theme', style: TextStyle(fontSize: 20.0)),
+                      DropdownButton<String>(
+                        value: themeValue,
+                        icon: Icon(Icons.arrow_downward),
+                        iconSize: 24,
+                        elevation: 16,
+                        style: TextStyle(color: Colors.deepPurple),
+                        underline: Container(
+                          height: 2,
+                          color: Colors.deepPurpleAccent,
+                        ),
+                        onChanged: (String newValue) {
+                          setState(() {
+                            themeValue = newValue;
+                          });
+                        },
+                        items: <String>['Light', 'Dark']
+                            .map<DropdownMenuItem<String>>((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child:
+                                Text(value, style: TextStyle(fontSize: 20.0)),
+                          );
+                        }).toList(),
+                      )
+                    ])
+              ]),
+              Column(children: [
+                Text('Notification Settings',
+                    style:
+                        TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold)),
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('Notifications', style: TextStyle(fontSize: 20.0)),
+                      Switch(
+                        value: isSwitched,
+                        onChanged: (value) {
+                          setState(() {
+                            isSwitched = value;
+                            print(isSwitched);
+                          });
+                        },
+                        activeTrackColor: Colors.lightGreenAccent,
+                        activeColor: Colors.green,
+                      )
+                    ])
+              ])
+            ]));
   }
 }
