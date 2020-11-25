20d8178feca154bf662249c51f80174d3fd9e4e6
Awet Ghebrelassie <awetghebreslassie@gmail.com>
2020-11-26 01:01:15 +0200

    Added dotenv package for environemntal variables, changed application ID, and other android build configurations

--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -3,6 +3,7 @@ import 'package:carePanda/model/question_item.dart';
 import 'package:carePanda/model/reportMsgModel.dart';
 import 'package:carePanda/model/survey_response.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'LocalStorageService.dart';
 import 'ServiceLocator.dart';
 
@@ -15,12 +16,6 @@ class FirestoreService {
 
   final FirebaseFirestore _db = FirebaseFirestore.instance;
 
-  // firestore collection path values
-  static const String _survey_response_path = "survey_responses";
-  static const String _survey_questions_path = "survey_questions";
-  static const String _hrMessages_path = "hr_messages";
-  static const String _reportBug_path = "bug_report_mesages";
-
   factory FirestoreService() {
     return _firestoreService;
   }
@@ -30,7 +25,7 @@ class FirestoreService {
   // get all responses or answeres submitted by all users
   Stream<List<SurveyResponse>> getAllSurveyResponses() {
     return _db
-        .collection(_survey_response_path)
+        .collection(DotEnv().env['_survey_response_path'])
         // .get() //future
         .snapshots()
         .map((snapshots) => snapshots.docs
@@ -44,7 +39,7 @@ class FirestoreService {
   */
   Stream<List<SurveyResponse>> getCurrentUserSurveyResponses() {
     return _db
-        .collection(_survey_response_path)
+        .collection(DotEnv().env['_survey_response_path'])
         .where("userId",
             isEqualTo: locator<LocalStorageService>().anonymousUserId)
         .snapshots()
@@ -55,14 +50,18 @@ class FirestoreService {
 
   // save single response or answer to Firestore
   Future<void> saveSurveyResponse(SurveyResponse response) {
-    return _db.collection(_survey_response_path).doc().set(response.toMap());
+    return _db
+        .collection(DotEnv().env['_survey_response_path'])
+        .doc()
+        .set(response.toMap());
   }
 
   // save a list or collection of answeres at once to Firestore
   Future<void> saveAllSurveyResponse(List<SurveyResponse> responses) {
     WriteBatch batch = _db.batch();
     for (final res in responses) {
-      DocumentReference _docRef = _db.collection(_survey_response_path).doc();
+      DocumentReference _docRef =
+          _db.collection(DotEnv().env['_survey_response_path']).doc();
       batch.set(_docRef, res.toMap());
     }
 
@@ -76,7 +75,7 @@ class FirestoreService {
       QuestionItem from map values
     */
     return _db
-        .collection(_survey_questions_path)
+        .collection(DotEnv().env['_survey_questions_path'])
         // .get() //future
         .snapshots()
         .map((snapshots) => snapshots.docs
@@ -90,7 +89,7 @@ class FirestoreService {
   // get single question by id from survey_questions collection
   Stream<QuestionItem> getQuestionById(String questionId) {
     return _db
-        .collection(_survey_questions_path)
+        .collection(DotEnv().env['_survey_questions_path'])
         .doc(questionId)
         .snapshots()
         .map((snapshots) => QuestionItem.fromMap({
@@ -101,28 +100,40 @@ class FirestoreService {
 
   // Save new questionnaire
   Future<void> saveQuestionnaire(QuestionItem response) {
-    return _db.collection(_survey_questions_path).doc().set(response.toMap());
+    return _db
+        .collection(DotEnv().env['_survey_questions_path'])
+        .doc()
+        .set(response.toMap());
   }
 
   // Edit questionnaire
   Future<void> editQuestionnaire(QuestionItem response, id) {
-    return _db.collection(_survey_questions_path).doc(id).set(response.toMap());
+    return _db
+        .collection(DotEnv().env['_survey_questions_path'])
+        .doc(id)
+        .set(response.toMap());
   }
 
   // Delete questionnaire
   Future<void> deleteQuestionnaire(id) {
-    return _db.collection(_survey_questions_path).doc(id).delete();
+    return _db
+        .collection(DotEnv().env['_survey_questions_path'])
+        .doc(id)
+        .delete();
   }
 
   // Save new HR message
   Future<void> saveHrMessage(MsgDataStructure response) {
-    return _db.collection(_hrMessages_path).doc().set(response.toMap());
+    return _db
+        .collection(DotEnv().env['_hrMessages_path'])
+        .doc()
+        .set(response.toMap());
   }
 
   // Get all HR messages
   Stream<List<MsgDataStructure>> getAllHrMessages() {
     return _db
-        .collection(_hrMessages_path)
+        .collection(DotEnv().env['_hrMessages_path'])
         // .get() //future
         .snapshots()
         .map((snapshots) => snapshots.docs
@@ -135,11 +146,14 @@ class FirestoreService {
 
   // Delete HR message
   Future<void> deleteHrMessage(id) {
-    return _db.collection(_hrMessages_path).doc(id).delete();
+    return _db.collection(DotEnv().env['_hrMessages_path']).doc(id).delete();
   }
 
   // Save new report a bug -message
   Future<void> saveReportBugMsg(ReportMsgModel response) {
-    return _db.collection(_reportBug_path).doc().set(response.toMap());
+    return _db
+        .collection(DotEnv().env['_reportBug_path'])
+        .doc()
+        .set(response.toMap());
   }
 }
